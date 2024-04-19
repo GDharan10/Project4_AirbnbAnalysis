d4b1239e7534a2d7a7246b8417f8d62f32485965
Giri Dharan <161794250+GDharan10@users.noreply.github.com>
2024-04-19 13:43:50 +0530

    Add files via upload

--- Airbnb.py
+++ Airbnb.py
@@ -1,7 +1,5 @@
 import pandas as pd
-
 import plotly.express as px
-
 import streamlit as st
 from streamlit_option_menu import option_menu
 
@@ -38,40 +36,36 @@ if menu == 'Home':
 
 if menu == 'Analysis':
     st.markdown(f""" <style>.stApp {{
-                background: url('https://media.cntraveller.com/photos/63bd91b73c7bca633cbfe0fb/4:3/w_2400,h_1800,c_limit/airbnb.jpg');   
+                background: url('https://news.airbnb.com/wp-content/uploads/sites/4/2023/11/PJMPHOTO18Q436_SNYA72_0609.jpg?fit=2500%2C1667');   
                 background-size: cover}}
                 </style>""",unsafe_allow_html=True)
 
     with row1[1]:
-        analysis = st.selectbox("", ["1. Country Distribution",
-                                    "2. ",
-                                    "3. ",
-                                    "4. ",
-                                    "5. ",
-                                    "6. ",
-                                    "7. ",
-                                    "8. ",
-                                    "9. ",
-                                    "10. "
-                                    ])
+        analysis = st.selectbox("Exploratory Analysis of Airbnb Listing Data", ["Distribution of Listings by Country",
+                                                                        "Price Variation by Selected Feature",
+                                                                        "Frequency of Selected Feature",
+                                                                        "Sum of Price and Beds by Number of Bedrooms",
+                                                                        "Average Price by Cancellation Policy",
+                                                                        "Sum of Price and Beds by Property Type",
+                                                                        "Sum of Price and Guests Included by Cancellation Policy",
+                                                                        "Sum of Price and Average Review Scores Rating by Number of Bathrooms"
+                                                                        ])
     
-    if analysis == "1. Country Distribution":
+    if analysis == "Distribution of Listings by Country":
 
         sub_columns = st.columns(2)
 
         with sub_columns[0]:
 
-            # Assuming df is your DataFrame containing the 'country' column
             country_counts = df['country'].value_counts().reset_index()
             country_counts.columns = ['country', 'count']
 
-            # Create a pie chart using Plotly Express
             fig = px.pie(country_counts, values='count', names='country', title='Country Distribution')
             fig.update_layout(title_x=0.3)
             st.plotly_chart(fig)
     
 
-    if analysis == "2. ":
+    if analysis == "Price Variation by Selected Feature":
 
         sub_columns = st.columns(2)
 
@@ -80,16 +74,112 @@ if menu == 'Analysis':
 
                     with sub_columns_1[1]:
                         yaxis = st.selectbox("", ['price',
-                                                  "review_scores_rating",
-                                                  "extra_people_fee",                                                  
+                                                  'security_deposit',
+                                                  'cleaning_fee',
+                                                  'extra_people_fee',
                                                 ])
 
         with sub_columns[0]:
-            fig = px.box(df, x='country', y = yaxis, title=f"{yaxis} by Country")
-            fig.update_layout(xaxis_title='Country', yaxis_title = yaxis , xaxis_tickangle=-45)
-            fig.update_layout(title_x=0.35)
+            fig = px.bar(df, x='country', y = yaxis, title=f"{yaxis} by Country")
+            fig.update_layout(xaxis_title='Country', yaxis_title = yaxis , xaxis_tickangle=-45, title_x=0.35)
             st.plotly_chart(fig)
 
+    if analysis == "Frequency of Selected Feature":
+
+        sub_columns = st.columns(2)
+
+        with sub_columns[1]:
+                    sub_columns_1 = st.columns(2)
+
+                    with sub_columns_1[1]:
+                        yaxis = st.selectbox("", ['property_type',
+                                                  "country",
+                                                  "bed_type",
+                                                ])
+
+        with sub_columns[0]:
+            property_type_counts = df[yaxis].value_counts().reset_index()
+            property_type_counts.columns = [yaxis, 'count']
+
+            fig = px.bar(property_type_counts, x='count', y=yaxis, title=f'Frequency of {yaxis}', orientation='h')
+            fig.update_layout(xaxis_title='Frequency', yaxis_title= yaxis, xaxis_tickangle=-45, title_x=0.35)
+            st.plotly_chart(fig)
+    
+    if analysis == "Sum of Price and Beds by Number of Bedrooms":
+
+        agg_df = df.groupby('bedrooms').agg({'price': 'sum', 'beds': 'sum'}).reset_index()
+
+        # Create a bar-line chart with Plotly Express
+        fig = px.bar(agg_df, x='bedrooms', y='price', title='Sum of Price and Sum of Beds by Number of Bedrooms')
+        fig.add_scatter(x=agg_df['bedrooms'], y=agg_df['beds'], mode='lines', name='Sum of Beds', yaxis='y2')
+
+        # Update layout
+        fig.update_layout(
+            xaxis_title='Number of Bedrooms',
+            yaxis=dict(title='Sum of Price', side='left', color='blue'),
+            yaxis2=dict(title='Sum of Beds', side='right', overlaying='y', color='green')
+        )
+
+        # Display the Plotly chart using st.plotly_chart()
+        st.plotly_chart(fig)
+    
+    if analysis == "Average Price by Cancellation Policy":
+        avg_price_by_cancellation = df.groupby('cancellation_policy')['price'].mean().reset_index()
+        bar_fig = px.bar(avg_price_by_cancellation, x='cancellation_policy', y='price', title='Average Price by Cancellation Policy')
+        bar_fig.update_xaxes(title='Cancellation Policy')
+        bar_fig.update_yaxes(title='Average Price')
+        st.plotly_chart(bar_fig)
+    
+    if analysis == "Sum of Price and Beds by Property Type":
+        # Aggregate the data by property type
+        agg_property_df = df.groupby('property_type').agg({'price': 'sum', 'beds': 'sum'}).reset_index()
+
+        # Create a bar-line chart for property type
+        property_fig = px.bar(agg_property_df, x='property_type', y='price', title='Sum of Price and Sum of Beds by Property Type')
+        property_fig.add_scatter(x=agg_property_df['property_type'], y=agg_property_df['beds'], mode='lines', name='Sum of Beds', yaxis='y2')
+
+        # Update layout for property type
+        property_fig.update_layout(
+            xaxis_title='Property Type',
+            yaxis=dict(title='Sum of Price', side='left', color='blue'),
+            yaxis2=dict(title='Sum of Beds', side='right', overlaying='y', color='green')
+        )
+        st.plotly_chart(property_fig)
+
+    if analysis == "Sum of Price and Guests Included by Cancellation Policy":
+        # Aggregate the data by cancellation policy
+        agg_cancel_df = df.groupby('cancellation_policy').agg({'price': 'sum', 'guests_included': 'sum'}).reset_index()
+
+        # Create a bar-line chart for cancellation policy
+        cancel_fig = px.bar(agg_cancel_df, x='cancellation_policy', y='price', title='Sum of Price and Sum of Guests Included by Cancellation Policy')
+        cancel_fig.add_scatter(x=agg_cancel_df['cancellation_policy'], y=agg_cancel_df['guests_included'], mode='lines', name='Sum of Guests Included', yaxis='y2')
+
+        # Update layout for cancellation policy
+        cancel_fig.update_layout(
+            xaxis_title='Cancellation Policy',
+            yaxis=dict(title='Sum of Price', side='left', color='blue'),
+            yaxis2=dict(title='Sum of Guests Included', side='right', overlaying='y', color='green')
+        )
+        st.plotly_chart(cancel_fig)
+
+    if analysis == "Sum of Price and Average Review Scores Rating by Number of Bathrooms":
+        # Aggregate the data by number of bathrooms
+        agg_bathroom_df = df.groupby('bathrooms').agg({'price': 'sum', 'review_scores_rating': 'mean'}).reset_index()
+
+        # Create a bar-line chart for number of bathrooms
+        bathroom_fig = px.bar(agg_bathroom_df, x='bathrooms', y='price', title='Sum of Price and Average Review Scores Rating by Number of Bathrooms')
+        bathroom_fig.add_scatter(x=agg_bathroom_df['bathrooms'], y=agg_bathroom_df['review_scores_rating'], mode='lines', name='Average Review Scores Rating', yaxis='y2')
+
+        # Update layout for number of bathrooms
+        bathroom_fig.update_layout(
+            xaxis_title='Number of Bathrooms',
+            yaxis=dict(title='Sum of Price', side='left', color='blue'),
+            yaxis2=dict(title='Average Review Scores Rating', side='right', overlaying='y', color='green')
+        )
+
+        # Display the Plotly charts
+
+        st.plotly_chart(bathroom_fig)
 
 
 
