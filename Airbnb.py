4ea409993f73a20c8527591c918dd332b9ffe63c
Giri Dharan <161794250+GDharan10@users.noreply.github.com>
2024-04-19 11:06:09 +0530

    Add files via upload

--- Airbnb.py
+++ Airbnb.py
@@ -4,12 +4,14 @@ import seaborn as sns
 import streamlit as st
 from streamlit_option_menu import option_menu
 
+df = pd.read_csv("C:\\GD\\Notes\\DS Class\\DTM15\\Project\\Guvi project\\4 Airbnb\\AirbnbDataset.csv")
+
 st.set_page_config(
-    page_title="Airbnb Analysis",
-    page_icon="https://static-00.iconduck.com/assets.00/airbnb-icon-512x512-d9grja5t.png",
-    layout="wide",
-    initial_sidebar_state="collapsed"
-)
+                    page_title="Airbnb Analysis",
+                    page_icon="https://static-00.iconduck.com/assets.00/airbnb-icon-512x512-d9grja5t.png",
+                    layout="wide",
+                    initial_sidebar_state="collapsed"
+                    )
 
 row1 = st.columns(2)
 with row1[0]:
@@ -17,11 +19,11 @@ with row1[0]:
 
     with sub_columns[0]:
         menu = option_menu("Menu", ["Home","Analysis"], 
-                icons=["house", "graph-up"],
-                menu_icon="cast",
-                default_index=0,
-                styles={"icon": {"color": "orange", "font-size": "20px"}}
-                )
+                            icons=["house", "graph-up"],
+                            menu_icon="cast",
+                            default_index=0,
+                            styles={"icon": {"color": "orange", "font-size": "20px"}}
+                            )
 
 
 if menu == 'Home':
@@ -33,5 +35,17 @@ if menu == 'Home':
     
 
 if menu == 'Analysis':
-    with row1[1]:
-        st.image('https://www.stocksbnb.com/wp-content/uploads/2021/08/airbnb-678x381-1.png', use_column_width=True)
+
+    st.markdown(f""" <style>.stApp {{
+                    background: url('https://blog.artonemfg.com/hubfs/airbnb-3399753_1920.jpg');   
+                    background-size: cover}}
+                    </style>""",unsafe_allow_html=True)
+    
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.hist(df['review_scores_rating'], bins=20, color='skyblue')
+    ax.set_xlabel('Review Scores Rating')
+    ax.set_ylabel('Frequency')
+    ax.set_title('Review Scores Rating Distribution')
+
+    # Display the Matplotlib plot using st.pyplot(), passing the figure explicitly
+    st.pyplot(fig)
